d2f42857084499edd725a35af392633f84f62621
Mathew Cormier <mathew.corm@gmail.com>
2019-02-20 18:37:21 -0500

    Add group functions

--- contracts/CounterApp.sol
+++ contracts/CounterApp.sol
@@ -6,14 +6,22 @@ import "@aragon/os/contracts/lib/math/SafeMath.sol";
 contract CounterApp is AragonApp {
     using SafeMath for uint256;
 
-    /// Events
-    event Increment(address indexed entity, uint256 step);
-    event Decrement(address indexed entity, uint256 step);
+    /**
+     * Represents a group and its entities within it   
+     */
+    struct Group {
+        string groupName;
+        mapping (address => uint) entitiesWithIndex;
+        address[] entities;
+        bool exists;
+    }
 
-    /// State
+    event GroupChange(uint256 groupId);
+
+    Group[] private groups;
     uint256 public value;
 
-    /// ACL
+    bytes32 constant public MANAGER_ROLE = keccak256("MANAGER_ROLE");
     bytes32 constant public INCREMENT_ROLE = keccak256("INCREMENT_ROLE");
     bytes32 constant public DECREMENT_ROLE = keccak256("DECREMENT_ROLE");
 
@@ -22,20 +30,91 @@ contract CounterApp is AragonApp {
     }
 
     /**
-     * @notice Increment the counter by `step`
-     * @param step Amount to increment by
+     * @notice Create a new group
+     * @param _groupName Name of the group
      */
-    function increment(uint256 step) auth(INCREMENT_ROLE) external {
-        value = value.add(step);
-        emit Increment(msg.sender, step);
+    function createGroup(string _groupName) external auth(MANAGER_ROLE) {
+        uint id = groups.length;
+        groups[id].groupName = _groupName;
+        groups[id].exists = true;
+        emit GroupChange(groups.length - 1);
     }
 
     /**
-     * @notice Decrement the counter by `step`
-     * @param step Amount to decrement by
+     * @notice Delete a group
+     * @param _groupId Id of the group to delete
      */
-    function decrement(uint256 step) auth(DECREMENT_ROLE) external {
-        value = value.sub(step);
-        emit Decrement(msg.sender, step);
+    function deleteGroup(uint256 _groupId) external auth(MANAGER_ROLE) {
+        require(groups[_groupId].exists);
+        delete groups[_groupId];
+        emit GroupChange(_groupId);
     }
+
+    /**
+     * @notice Rename a group
+     * @param _groupId Id of the group to rename
+     * @param _newGroupName New name for the group
+     */
+    function renameGroup(uint256 _groupId, string _newGroupName) external auth(MANAGER_ROLE) {
+        require(groups[_groupId].exists);
+        groups[_groupId].groupName = _newGroupName;
+        emit GroupChange(_groupId);
+    }
+
+    /**
+     * @notice Get a specific group
+     * @param _groupId Id of the group to return
+     */
+    function getGroup(uint256 _groupId) public view returns (address[] entities, string name) {        
+        entities = groups[_groupId].entities;
+        name = groups[_groupId].groupName;
+    }
+
+    /**
+     * @notice Get a list of all the groups Id's
+     */
+     /*
+    function getGroupIds() public view returns (uint[]) {
+        return groups.groupList;
+    }*/
+
+    /**
+     * @dev Returns the group count
+     */
+    function groupCount() external view returns (uint256) {
+        return groups.length - 1;
+    }
+
+    /**
+     * @notice Add an entity to a group
+     * @param _groupId Id of the group to add the entity in
+     * @param _entity Address of the entity
+     */
+    function addEntityToGroup(uint256 _groupId, address _entity) 
+        auth(MANAGER_ROLE)
+        external 
+    {
+        require(groups[_groupId].exists);
+        groups[_groupId].entitiesWithIndex[_entity] = groups[_groupId].entities.length.add(1);
+        groups[_groupId].entities.push(_entity);
+        emit GroupChange(_groupId);
+    }
+
+    /**
+     * @notice Remove an entity from a group
+     * @param _groupId Id of the group to remove the entity from 
+     * @param _entity Address of the entity
+     */
+    function removeEntityFromGroup(uint256 _groupId, address _entity) 
+        auth(MANAGER_ROLE)
+        external 
+    {
+        uint indexOfEntity = groups[_groupId].entitiesWithIndex[_entity];
+        if (indexOfEntity > 0) {
+            indexOfEntity = indexOfEntity.sub(1);
+            delete groups[_groupId].entities[indexOfEntity];
+            delete groups[_groupId].entitiesWithIndex[_entity];
+        }
+    }
+
 }
